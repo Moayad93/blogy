9e1ed9395e9ccd9f597b8cda0b8277130a5c16ed
Moayad Alnuwaysir <moayad.alnu@gmail.com>
2019-12-05 13:38:30 +0300

    Add articles route's comments

--- app/routes/articles.js
+++ app/routes/articles.js
@@ -9,5 +9,40 @@ const
 const
   router = express.Router();
 
+/*********************************
+ * Action     : INDEX            *
+ * Method     : GET              *
+ * URI        : /api/articles    *
+ * Description: Get All Articles *
+ *********************************/
+
+/********************************************************
+ * Action     : SHOW                                    *
+ * Method     : GET                                     *
+ * URI        : /api/articles/66ftr54t8fu4rr78sww9r334r *
+ * Description: Get an Article by Article ID            *
+ ********************************************************/
+
+/*************************************
+ * Action     : CREATE               *
+ * Method     : POST                 *
+ * URI        : /api/articles        *
+ * Description: Create a new Article *
+ *************************************/
+
+/********************************************************
+ * Action     : UPDATE                                  *
+ * Method     : PATCH                                   *
+ * URI        : /api/articles/66ftr54t8fu4rr78sww9r334r *
+ * Description: Update an Article by Article ID         *
+ ********************************************************/
+
+/********************************************************
+ * Action     : DESTROY                                 *
+ * Method     : DELETE                                  *
+ * URI        : /api/articles/66ftr54t8fu4rr78sww9r334r *
+ * Description: Delete an Article by Article ID         *
+ ********************************************************/
+
 // Export the Router so we can use it in the server.js file
 module.exports = router;
