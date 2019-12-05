8638b487921a8baa75840fe11f2febe6913a7b6f
Moayad Alnuwaysir <moayad.alnu@gmail.com>
2019-12-05 12:04:58 +0300

    Update server.js comments

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+/****************************************************************
+ * #	Action 	URL      	 HTTP Verb	mongoose method             *
+ * 1	Index  	/xxxx    	 GET      	Xxx.find({})                *
+ * 2	Show   	/xxxx/:id	 GET      	Xxx.findById(req.params.id) *
+ * 3	Create 	/xxxx    	 POST     	Xxx.create(req.body)        *
+ * 4	Update 	/xxxx/:id	 PUT      	Xxx.findByIdAndUpdate()     *
+ * 5	Destroy	/xxxx    	 DELETE   	Xxx.findByIdAndRemove()     *
+ ****************************************************************/
+
 /****************************
  * Set up and Configuration *
  ****************************/
