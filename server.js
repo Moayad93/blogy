303c67551e7563e1e65ed624895fb63660c23613
Moayad Alnuwaysir <moayad.alnu@gmail.com>
2019-12-05 11:31:12 +0300

    Start the server on a given port

--- server.js
+++ server.js
@@ -10,3 +10,8 @@ const
 // Define PORT for the API to run on. Use NodeJS environment
 const
   port = process.env.PORT || 5000;
+
+// Start the server to listen for requests on a given port
+app.listen(port, () => {
+  console.log(`Blogy's server started on ${port}`);
+});
